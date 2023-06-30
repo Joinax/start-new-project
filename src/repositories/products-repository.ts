1524ba439967997977f220526b13af97cb381362
Joinax <fintroll18@gmail.com>
2023-06-30 23:44:09 +0300

    add meat

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -3,7 +3,7 @@ type ProductType ={
   id: string
 }
 
-let products: ProductType[] = [{id: '1', title: 'tomato'}, {id: '2', title: 'orange'}, {id: '3', title: 'cucumber'}, {id: '4', title: 'potatoes'}]
+let products: ProductType[] = [{id: '1', title: 'tomato'}, {id: '2', title: 'orange'}, {id: '3', title: 'cucumber'}, {id: '4', title: 'potatoes'}, {id: '5', title: 'meat'}]
 export const productsRepository = {
   getProducts(): ProductType[] {
     return products
